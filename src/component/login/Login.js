ea067468b83d92eee6c7b2f33ee8d593c99e82c3
Triveni Pratik Patel (Chaudhari) <trivenic@cybage.com>
2021-08-18 16:32:28 +0530

    login,register forms created

--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -1,5 +1,51 @@
-function Login(){
-    return <>Login</>
+import { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [loginFormData, setLoginFormData] = useState({
+    userid: "",
+    password: "",
+  });
+
+  function handleInputChange({ target }) {
+    const { name, value } = target;
+    setLoginFormData({ ...loginFormData, [name]: value });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(loginFormData);
+  }
+  const { userid, password } = loginFormData;
+  return (
+    <>
+      <form className="LoginForm" onSubmit={handleSubmit}>
+        <div className="LoginForm-wrapper-div">
+          <label className="LoginForm-label">Enter User Id :</label>
+          <input
+            type="text"
+            name="userid"
+            value={userid}
+            onChange={handleInputChange}
+            className="LoginForm-input"
+          />
+          <label className="LoginForm-label">Enter Password :</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleInputChange}
+            className="LoginForm-input"
+          />
+          <button type="submit" className="LoginForm-button">
+            Login
+          </button>
+          <button type="reset" className="LoginForm-button">
+            Cancel
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Login;
--- src/component/register/Register.js
+++ src/component/register/Register.js
@@ -1,5 +1,69 @@
-function Register(){
-    return <>Register</>
+import { useState } from "react";
+import './Register.css';
+
+function Register() {
+  const [registerFormData, setRegisterFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    dateOfBirth: "",
+  });
+
+  function handleInputChange({ target }) {
+    const { name, value } = target;
+    setRegisterFormData({ ...registerFormData, [name]: value });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(registerFormData);
+  }
+  const { name, email, password,  dateOfBirth } = registerFormData;
+  return (
+    <>
+      <form className="RegisterForm" onSubmit={handleSubmit}>
+        <div className="RegisterForm-wrapper-div">
+          <label className="RegisterForm-label">Enter Name :</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleInputChange}
+            className="RegisterForm-input"
+          />
+          <label className="RegisterForm-label">Enter Email :</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+            className="RegisterForm-input"
+          />
+          <label className="RegisterForm-label">Enter Password :</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleInputChange}
+            className="RegisterForm-input"
+          />
+          <label className="RegisterForm-label">Enter Date of Birth :</label>
+          <input
+            type="date"
+            name="dateOfBirth"
+            value={dateOfBirth}
+            onChange={handleInputChange}
+            className="RegisterForm-input"
+          />
+          <button type="submit" className="RegisterForm-button">
+            Login
+          </button>
+          <button type="reset" className="RegisterForm-button">
+            Cancel
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Register;
